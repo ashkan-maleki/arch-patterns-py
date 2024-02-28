1aa378f43e8ec7d35edaf2a36fba62aa905b6079
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-02-28 18:17:37 +0330

    add orm and repository

---
+++ repository.py
@@ -0,0 +1,28 @@
+import abc
+import model
+from typing import List
+from sqlalchemy.orm import Session
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch) -> None:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def get(self, reference: str) -> model.Batch:
+        raise NotImplementedError
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session) -> None:
+        self.session = session
+        
+    def add(self, batch: model.Batch) -> None:
+        self.session.add(batch)
+        
+    def get(self, reference: str) -> model.Batch:
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+    
+    def list(self) -> List[model.Batch]:
+        return self.session.query(model.Batch).all()
+    
