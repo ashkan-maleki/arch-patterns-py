bc36a2a151033ba87fd333c0ce94dc5ad14d37fc
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-02-29 22:17:41 +0330

    add flask api and application service layer and unit test

---
+++ services.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import List
+
+from sqlalchemy.orm import Session
+
+import model
+from model import OrderLine
+from repository import AbstractRepository
+
+
+class InvalidSku(Exception):
+    pass
+
+def is_valid_sku(sku: str, batches: List[model.Batch]):
+    return sku in {b.sku for b in batches}
+
+def allocate(line: OrderLine, repo: AbstractRepository, session: Session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref
