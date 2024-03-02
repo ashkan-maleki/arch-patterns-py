c7aed45206119dabb02b83e21d98f2cc49c22081
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-03-02 19:34:24 +0330

    fix bug add text method of sql alchemy to call execute

--- test_orm.py
+++ test_orm.py
@@ -1,13 +1,14 @@
 import model
 from datetime import date
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 
 def test_orderline_mapper_can_load_lines(session: Session):
     session.execute(
-        "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+        text("INSERT INTO order_lines (orderid, sku, qty) VALUES "
         '("order1", "RED-CHAIR", 12),'
         '("order1", "RED-TABLE", 13),'
-        '("order2", "BLUE-LIPSTICK", 14)'
+        '("order2", "BLUE-LIPSTICK", 14)')
     )
     expected = [
         model.OrderLine("order1", "RED-CHAIR", 12),
