886719353dc3817d06a4d906fbec39567bddc535
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-03-02 21:21:01 +0330

    Fix bug - add text method sql code in test repo

--- test_repository.py
+++ test_repository.py
@@ -3,6 +3,7 @@ import model
 import repository
 
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 
 
 def test_repository_can_save_a_batch(session: Session):
@@ -12,37 +13,41 @@ def test_repository_can_save_a_batch(session: Session):
     repo.add(batch)
     
     rows = session.execute(
-        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+        text('SELECT reference, sku, _purchased_quantity, eta FROM "batches"')
     )
     assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
     
 def insert_order_line(session: Session):
     session.execute(
-        "INSERT INTO order_lines (orderid, sku, qty)"
+        text(
+            "INSERT INTO order_lines (orderid, sku, qty)"
         ' VALUES ("order1", "GENERIC-SOFA", 12)'
+        )
     )
     [[order_line_id]] = session.execute(
-        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
+        text("SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku"),
         dict(orderid="order1", sku="GENERIC-SOFA"),
     )
     return order_line_id
 
 def insert_batch(session: Session, batch_id: str) -> str:
     session.execute(
-        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
-        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
+        text("INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)'),
         dict(batch_id=batch_id),
     )
     [[batch_id]] = session.execute(
-        'SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"',
+        text('SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"'),
         dict(batch_id=batch_id),
     )
     return batch_id
 
 def insert_allocation(session: Session, orderline_id: int, batch_id: int):
     session.execute(
-        "INSERT INTO allocations (orderline_id, batch_id)"
-        " VALUES (:orderline_id, :batch_id)",
+        text(
+            "INSERT INTO allocations (orderline_id, batch_id)"
+        " VALUES (:orderline_id, :batch_id)"
+        ),
         dict(orderline_id=orderline_id, batch_id=batch_id),
     )
     
