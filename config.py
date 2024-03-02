1ea2a28b306843a9f4035109ed1a76bfd2090a36
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-03-02 22:29:24 +0330

    fix bug - modify docker build

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ def get_progress_uri():
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 def get_api_url():
+    
     host = os.environ.get("API_HOST", "localhost")
     port = 5005 if host == "localhost" else 80
     return f"http://{host}:{port}"
