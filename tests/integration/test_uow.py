8d01d01a41f30388783141158578e5ecfb17ae02
Mamal Maleki <mohammadalimalekie@gmail.com>
2024-03-23 15:00:16 +0330

    add sql alch text to test uow

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -41,9 +41,9 @@ def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
     
     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
     with uow:
-        batch : model.Batch = uow.batches.get(reference="batch1")
+        product = uow.products.get(sku="HIPSTER-WORKBENCH")
         line = model.OrderLine("o1", "HIPSTER-WORKBENCH", 10)
-        batch.allocate(line)
+        product.allocate(line)
         uow.commit()
         
     batchref = get_allocated_batch_ref(session, "o1", "HIPSTER-WORKBENCH")
